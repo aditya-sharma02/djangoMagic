f24832c9f7f2b6c7d382178d540903173d3fe41c
aditya-sharma02 <adyashma786@gmail.com>
2023-06-01 13:35:25 +0530

    employee model

--- api/models.py
+++ api/models.py
@@ -15,3 +15,18 @@ class Company(models.Model):
         ))
     added_date = models.DateTimeField(auto_now=True)
     active = models.BooleanField(default=True)
+
+
+# createing employee Model
+class Employe(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    address = models.CharField(max_length=100)
+    phone = models.CharField(max_length=10)
+    about = models.TextField()
+    position = models.CharField(max_length=100,choices=(
+        ('Manager','manager'),
+        ('Software Developer','sd'),
+        ('Porject Leader','pl')
+        ))
+    company = models.ForeignKey(Company,on_delete=models.CASCADE)
