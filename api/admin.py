9d65adf884d12c516dac8a4e1c9a1001cbb85392
aditya-sharma02 <adyashma786@gmail.com>
2023-06-01 21:45:19 +0530

    admin

--- api/admin.py
+++ api/admin.py
@@ -6,9 +6,9 @@ from api.models import Company,Employee
 
 class CompanyAdmin(admin.ModelAdmin):
     list_display=('name','location','type')
-
-
-
+    search_fields=('name',)
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display=('name','company','email')
 
 admin.site.register(Company,CompanyAdmin)
-admin.site.register(Employee)
+admin.site.register(Employee,EmployeeAdmin)
